397ff84187c954ea21280bbe5b7c0b7c53b3c3b3
deekshithanand <deekshith27anand@gmail.com>
2020-05-09 13:44:23 +0530

    Updates

---
+++ precache-manifest.9e0d99360d72f345ac1031acaaa0c7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122d3b39b62ef67d518ab95a498c8d1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cd1224406fd310881d0",
+    "url": "./static/css/main.e9f30423.chunk.css"
+  },
+  {
+    "revision": "3ee6d2488f0c8794f0f7",
+    "url": "./static/js/2.444b78f1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.444b78f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd1224406fd310881d0",
+    "url": "./static/js/main.247f97b6.chunk.js"
+  },
+  {
+    "revision": "fed96213b7e741e377c1",
+    "url": "./static/js/runtime-main.073f6b70.js"
+  }
+]);
